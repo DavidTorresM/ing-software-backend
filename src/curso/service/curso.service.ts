e1402cca8658f1a02d1639d88d998d707d149581
David <david.tor.prog@hotmail.com>
2021-01-11 17:18:59 -0600

    PSC-13 Servicio de curso terminado

--- src/curso/service/curso.service.ts
+++ src/curso/service/curso.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {Repository } from 'typeorm'
+
+import { Curso } from '../curso.entity';
+import { CursoDTO } from '../interface/curso.interface';
 
 @Injectable()
-export class CursoService {}
+export class CursoService {
+    constructor(
+      @InjectRepository(Curso) private repositorioCurso: Repository< Curso >
+    ) {}
+    async obtenerCursos(): Promise< Curso[] >{
+        const cursos = await this.repositorioCurso.find();
+        return cursos;
+    }
+    async obtenerCurso(id: string): Promise <Curso | null> {
+        const curso = await this.repositorioCurso.findOne({
+            id,
+        });
+        return curso;
+    }
+    async crear(curso: CursoDTO): Promise< Curso >{
+        const nuevoCurso = this.repositorioCurso.create(curso);
+        return this.repositorioCurso.save(nuevoCurso);
+    }
+}
