16ae5e3a6fe2924e73dc55185456014c6307db10
David <david.tor.prog@hotmail.com>
2021-01-11 19:05:07 -0600

    Service administrador =)

--- src/administrador/controller/administrador.controller.ts
+++ src/administrador/controller/administrador.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+  } from '@nestjs/common'
+  
+  import { Administrador } from '../administrador.entity';
+  import { AdministradorDTO } from '../interface/administrador.interface';
+  import { AdministradorService } from '../service/administrador.service';
+  
+@Controller('api/administrador')
+export class AdministradorController {
+    constructor(private servicioAdministrador: AdministradorService){}
+    @Post('crear')
+    async crearAdministrador(@Body() administradorDTO: AdministradorDTO): Promise< Administrador > {
+      const respuesta = await this.servicioAdministrador.crear(administradorDTO);
+      
+      return respuesta;
+    }
+    
+    @Get('buscar/:id')
+    async obtenerAdministrador(@Param('id') id: string): Promise< Administrador | null >{
+      const respuesta = await this.servicioAdministrador.obtenerAdministrador(id);
+      
+      delete respuesta.usuario.id;
+      delete respuesta.usuario.idDireccion;
+      delete respuesta.usuario.direccion.id;
+      delete respuesta.usuario.direccion.idColonia;
+      delete respuesta.usuario.direccion.idDelegacion;
+      delete respuesta.usuario.direccion.idEstado;
 
-@Controller('administrador')
-export class AdministradorController {}
+      return respuesta;
+    }
+  
+    @Get('listar')
+    async obtenerAdministradores() : Promise< Administrador[] > {
+      const respuesta = await this.servicioAdministrador.obtenerAdministradores();
+      respuesta.map(admin => {
+        delete admin.usuario.id;
+        delete admin.usuario.idDireccion;
+        delete admin.usuario.direccion.id;
+        delete admin.usuario.direccion.idColonia;
+        delete admin.usuario.direccion.idDelegacion;
+        delete admin.usuario.direccion.idEstado;
+      });
+      return respuesta;
+    }
+}
---
+++ src/administrador/interface/administrador.interface.ts
@@ -0,0 +1,4 @@
+export interface AdministradorDTO {
+  idUsuario: string;
+}
+  
