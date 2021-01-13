1b86151880a622a768d2e01e94a7c3712516c0c4
BrayanBntz <brayanbntzr@gmail.com>
2021-01-13 13:37:59 -0600

    funciones de creacion y busqueda de Publicacion

--- src/publicacion/service/publicacion.service.ts
+++ src/publicacion/service/publicacion.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {Repository } from 'typeorm';
+
+import { Publicacion } from '../publicacion.entity';
+import { PublicacionDTO } from '../interface/publicacion.interface';
 
 @Injectable()
-export class PublicacionService {}
+export class PublicacionService {
+	constructor(
+		@InjectRepository(Publicacion) private repositorioPublicacion: Repository< Publicacion >
+	){}
+
+	async crear(publicacion: PublicacionDTO):Promise< Publicacion >{
+		const nuevaPublicacion = this.repositorioPublicacion.create(publicacion);
+		console.log(nuevaPublicacion);
+		return this.repositorioPublicacion.save(nuevaPublicacion);
+	}
+
+	async obtenerPublicacion(id: number): Promise< Publicacion | null >{
+		const publicacion = await this.repositorioPublicacion.findOne({
+			id,
+		});
+
+		return publicacion;
+	}
+}
