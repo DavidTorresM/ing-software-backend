4fffa948a173689622e0c8e08ca8068c94cd2d50
DavidTorresM <dtorresm1200@alumno.ipn.mx>
2021-01-17 17:07:06 -0600

    Iniciando autenticacion Websockets

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -8,28 +8,11 @@ import {
   OnGatewayInit,
   WsException,
 } from '@nestjs/websockets';
-  import { from, Observable } from 'rxjs';
-  import { map } from 'rxjs/operators';
 import { MensajeDTO } from 'src/mensaje/interface/mensaje.interface';
-import { MensajeModule } from 'src/mensaje/mensaje.module';
-  
-/*
-import { JwtService } from '../auth/jwt/jwt.service';
-import { User } from '../users/interfaces/user.interface';
-import { RoomsService } from '../rooms/rooms.service';
-
-
-*/
-//import { Server } from 'socket.io';
 import { Socket, Server } from 'socket.io';
-
 import { MensajeService } from '../mensaje/service/mensaje.service';
 import { SalaService } from '../sala/service/sala.service';
-import { Sala } from '../sala/sala.entity';
-import { Mensaje } from 'src/mensaje/mensaje.entity';
-import { throws } from 'assert';
-
-
+import { SalaDTO } from 'src/sala/interface/sala.interface';
 
 @WebSocketGateway(8080,{ transports:['websocket'] })
 export class ChatGateway implements OnGatewayConnection,OnGatewayDisconnect, OnGatewayInit {
@@ -53,36 +36,36 @@ export class ChatGateway implements OnGatewayConnection,OnGatewayDisconnect, OnG
   handleDisconnect(){
     console.log("[*] Desconectando servidor");
   }
-  @SubscribeMessage('chatToServer')
+  @SubscribeMessage('chatServer')
   handleMessage(client: Socket, message: MensajeDTO
     //{ sender: string, room: string, message: string }
     ) {
     this.mensajeService.crear(message);
     console.log("chatToServer:\n",message);
-    this.server.to(message.idCurso).emit('chatToClient', message);
+    this.server.to(message.idCurso).emit('chatClient', message);
   }
 
-  //NOTA Recibe id curso
+  //NOTA Recibe el idCurso
   //salida el json de la sala a unirte
-  @SubscribeMessage('joinRoom')
-  async handleRoomJoin(client: Socket, room: string ) {
+  @SubscribeMessage('unirseSala')
+  async handleRoomJoin(client: Socket, room: SalaDTO ) {
     //verificando si al sala existe 
-    const sala =await this.salaService.obtenerSalaPorCampo("idCurso",room);
+    const sala =await this.salaService.obtenerSalaPorCampo("idCurso",room.idCurso);
     if(!sala){
       console.log("[info] La NO sala existe =)");
       throw new WsException('Invalid Room.');
     }
     console.log("[info] La sala existe =)");
     console.log("handleRoomJoin:\n",room);
-    client.join(room);
-    client.emit('joinedRoom', sala);
+    client.join(room.idCurso);
+    client.emit('unidoSala', sala);
   }
 
-  @SubscribeMessage('leaveRoom')
-  handleRoomLeave(client: Socket, room: string ) {
+  @SubscribeMessage('abandonarSala')
+  handleRoomLeave(client: Socket, room: SalaDTO ) {
     console.log("handleRoomLeave:\n",room);
-    client.leave(room);
-    client.emit('leftRoom', room);
+    client.leave(room.idCurso);
+    client.emit('abandonoSala', room);
   }
 
 }
