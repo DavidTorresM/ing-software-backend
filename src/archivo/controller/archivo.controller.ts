aaa22d2d6b57c99afabde23f3fa3743707edcb6f
Isaac Sanchez Aguilar <saais31@gmail.com>
2021-01-17 22:51:14 -0600

    Subir archivo a amazon s3

--- src/archivo/controller/archivo.controller.ts
+++ src/archivo/controller/archivo.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, Response } from '@nestjs/common';
 
-@Controller('archivo')
-export class ArchivoController {}
+import { ArchivoService } from '../service/archivo.service';
+
+@Controller('api/archivo')
+export class ArchivoController {
+  constructor(private servicioArchivo: ArchivoService) {}
+
+  @Post('subir')
+  async upload(@Request() request, @Response() response) {
+    try{
+      this.servicioArchivo.subirArchivo(request, response);
+    }
+    catch(error) {
+      return response.status(500).json(`Hubo un error al subir el archivo: ${error.message}`);
+    }
+  }
+}
--- src/archivo/service/archivo.service.ts
+++ src/archivo/service/archivo.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Request, Response, Injectable } from '@nestjs/common';
+import * as multer from  'multer';
+import * as AWS from 'aws-sdk';
+import * as multers3 from 'multer-s3';
+
+const s3 = new AWS.S3();
+AWS.config.update({
+  accessKeyId: 'AKIAJYK5ZVNOIX2B5FUQ',
+  secretAccessKey: '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',
+  region: 'us-east-2',
+});
 
 @Injectable()
-export class ArchivoService {}
+export class ArchivoService {
+  constructor(){}
+
+  upload = multer({
+    storage: multers3({
+      s3: s3,
+      bucket: 'archivos-polimeet',
+      acl: 'public-read',
+      key: (request, archivo, cb) => {
+        cb(null, `${Date.now().toString()} - ${archivo.originalname}`);
+      },
+    }),
+  }).array('archivo', 1);
+
+  async subirArchivo(@Request() req, @Response() res) {
+    try {
+      this.upload(req, res, err => {
+        if (err) {
+          console.log('Hubo un error en subirArchivo:', err.message);
+          return res.status(404).json(`Error al subir el archivo a amazon: ${err.message}`);
+        }
+
+        return res.status(201).json(req.files[0].location);
+      });
+    } catch (err) {
+      console.log('Error:', err.message);
+      return res.status(500).json(`Error al subir un archivo: ${err.message}`);
+    }
+  }
+}
